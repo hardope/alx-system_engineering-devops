7f40f1ba1a2fc6d3083d9491cb87eaaefdce52bf
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-04-22 22:07:15 +0100

    Create setup.sql

---
+++ 0x14-mysql/setup.sql
@@ -0,0 +1,10 @@
+CREATE USER holberton_user@localhost IDENTIFIED BY "projectcorrection280hbtn";
+GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
+CREATE DATABASE tyrell_corp;
+USE tyrell_corp;
+CREATE TABLE nexus6(id INTEGER, name TEXT);
+INSERT INTO nexus6 VALUES (0, "Jarvis");
+GRANT SELECT ON tyrell_corp.nexus6 TO holberton_user@localhost;
+CREATE USER replica_user@'%' IDENTIFIED BY "replica_user";
+GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';
+GRANT SELECT ON mysql.user TO holberton_user@localhost;
