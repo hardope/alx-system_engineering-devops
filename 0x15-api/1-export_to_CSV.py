c016ec84efd6369ce11308e62fac4c23213043ee
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-04-24 20:17:13 +0000

    Updated

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python3
-""" Python script that exports an API data into csv"""
+""" Export api to csv"""
 import csv
-import requests
 import sys
+import requests
+
 
 
 if __name__ == '__main__':
-    USER_ID = sys.argv[1]
-    url_to_user = 'https://jsonplaceholder.typicode.com/users/' + USER_ID
+    uid = sys.argv[1]
+    url_to_user = 'https://jsonplaceholder.typicode.com/users/' + uid
     res = requests.get(url_to_user)
-    """print(res.text)"""
     USERNAME = res.json().get('username')
-    """print(USERNAME)"""
     url_to_task = url_to_user + '/todos'
     res = requests.get(url_to_task)
     tasks = res.json()
-    """print(tasks)
-    for task in tasks:
-        TASK_COMPLETED_STATUS = task.get('completed')
-        print(TASK_COMPLETED_STATUS)
-        TASK_TITLE = task.get('title')
-        print(TASK_TITLE)
-    """
 
-    with open('{}.csv'.format(USER_ID), 'w') as csvfile:
+    with open('{}.csv'.format(uid), 'w') as csvfile:
         for task in tasks:
             TASK_COMPLETED_STATUS = task.get('completed')
             """print(TASK_COMPLETED_STATUS)"""
             TASK_TITLE = task.get('title')
             """print(TASK_TITLE)"""
             csvfile.write('"{}","{}","{}","{}"\n'.format(
+                uid, USERNAME, TASK_COMPLETED_STATUS, TASK_TITLE))
