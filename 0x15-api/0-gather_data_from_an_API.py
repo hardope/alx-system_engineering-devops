e69e813789d5f23735d400b963034afae5a17abe
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-04-24 20:07:35 +0000

    Updated

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -13,9 +13,9 @@ if __name__ == '__main__':
     if len(sys.argv) > 1:
         if re.fullmatch(r'\d+', sys.argv[1]):
             id = int(sys.argv[1])
-            req = requests.get('{}/users/{}'.format(REST_API, id)).json()
+            emp_req = requests.get('{}/users/{}'.format(REST_API, id)).json()
             task_req = requests.get('{}/todos'.format(REST_API)).json()
-            emp_name = req.get('name')
+            emp_name = emp_req.get('name')
             tasks = list(filter(lambda x: x.get('userId') == id, task_req))
             completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
             print(
