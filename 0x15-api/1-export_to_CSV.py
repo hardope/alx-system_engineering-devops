d930e48f118b78ca8a0e08f84de98e9a47f85bfe
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-04-24 20:20:43 +0000

    Updated

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,9 +8,9 @@ if __name__ == '__main__':
     uid = sys.argv[1]
     url = 'https://jsonplaceholder.typicode.com/users/' + uid
     res = requests.get(url)
-    USERNAME = res.json().get('username')
-    url_to_task = url + '/todos'
-    res = requests.get(url_to_task)
+    user = res.json().get('username')
+    task_url = url + '/todos'
+    res = requests.get(task_url)
     tasks = res.json()
 
     with open('{}.csv'.format(uid), 'w') as csvfile:
@@ -20,4 +20,4 @@ if __name__ == '__main__':
             TASK_TITLE = task.get('title')
             """print(TASK_TITLE)"""
             csvfile.write('"{}","{}","{}","{}"\n'.format(
-                uid, USERNAME, TASK_COMPLETED_STATUS, TASK_TITLE))
+                uid, user, TASK_COMPLETED_STATUS, TASK_TITLE))
