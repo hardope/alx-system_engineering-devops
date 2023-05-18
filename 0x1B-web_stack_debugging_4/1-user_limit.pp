cb297141b9952bc521240b1fd349ab9abb0671b4
Opeoluwa Adeyeri <adeyeriopeoluwa05@gmail.com>
2023-05-18 14:35:17 +0100

    Create 1-user_limit.pp

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,7 @@
+# Change the OS configuration so that it is possible to login with the
+# holberton user and open a file without any error message.
+
+exec {'OS security config':
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
+}
